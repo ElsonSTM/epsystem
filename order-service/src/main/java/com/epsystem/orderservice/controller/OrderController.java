96c5789f928c51cffff80e8b9f9249b949e84094
ElsonSTM <86759660+ElsonSTM@users.noreply.github.com>
2022-12-18 00:15:17 -0300

    Create microservice Order-Service (Controle, dto, repository, service and model) and communication database sql.

---
+++ order-service/src/main/java/com/epsystem/orderservice/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.epsystem.orderservice.controller;
+
+import com.epsystem.orderservice.dto.OrderRequest;
+import com.epsystem.orderservice.services.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}
---
+++ order-service/src/main/java/com/epsystem/orderservice/model/OrderLineItems.java
@@ -0,0 +1,24 @@
+package com.epsystem.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "t_order_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderLineItems {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
